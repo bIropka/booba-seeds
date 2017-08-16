c13da2c6cb9e90feeded78ace8c12ce196205ac4
bIropka <biropka@gmail.com>
2017-08-16 10:32:11 +0300

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,5 +1,17 @@
 $(window).ready(function() {
 
+    setTimeout(function() {
+        $('.preloader').fadeOut(500);
+    }, 500);
+
+    $(document).click(function(event) {
+
+        if (!$(event.target).closest(".custom-select").length) {
+            $('.custom-select').removeClass('active');
+        }
+
+    });
+
     if($(window).width() < 769) {
         $('.form-search').appendTo('header nav');
     } else {
@@ -55,8 +67,59 @@ $(window).ready(function() {
     });
 
     $('.catalog-burger').click(function() {
-
         $(this).parents('.catalog').toggleClass('active');
     });
+
+    $('.products-main-prev').click(function() {
+        if($(window).width() < 769) {
+            var margin = parseInt($('.product-card:first-of-type').css('margin-left'));
+            if(margin !== 250) {
+                $('.product-card:first-of-type').css('margin-left', margin + 234 + 'px');
+            }
+        }
+    });
+
+    $('.products-main-next').click(function() {
+        if($(window).width() < 769) {
+            var value;
+            if($(window).width() < 481) {
+                value = -(($('.products-list').find('.product-card').length - 2) * 234 - 16);
+            } else {
+                value = -(($('.products-list').find('.product-card').length - 4) * 234 - 16);
+            }
+            var margin = parseInt($('.product-card:first-of-type').css('margin-left'));
+            if(margin !== value) {
+                $('.product-card:first-of-type').css('margin-left', margin - 234 + 'px');
+            }
+        }
+    });
+
+    /*if($(window).width() < 769) {
+        $('.products-list').slick({
+            appendArrows: '.products-main-control',
+            prevArrow: '.products-main-prev',
+            nextArrow: '.products-main-next',
+            slidesToShow: 4,
+            infinite: false
+        });
+    } else {
+        $('.products-list').slick('unslick');
+    }
+
+    $(window).resize(function() {
+
+        if($(window).width() < 769) {
+            $('.products-list').slick({
+                appendArrows: '.products-main-control',
+                prevArrow: '.products-main-prev',
+                nextArrow: '.products-main-next',
+                slidesToShow: 4,
+                infinite: false
+            });
+        } else {
+            $('.products-list').slick('unslick');
+        }
+
+    });*/
 
 });
