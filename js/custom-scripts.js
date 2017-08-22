21fda37a0809f19df9da228b77581a86058aa68c
bIropka <biropka@gmail.com>
2017-08-22 11:50:27 +0300

    v 0.3 page-catalog.html added

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -66,9 +66,9 @@ $(window).ready(function() {
     $('.custom-select ul li').click(function() {
 
         var newCurrency = $(this).text();
-        var oldCurrency = $(this).parents('.custom-select').find('.current-value').text();
+        var oldCurrency = $(this).parents('.custom-select').find('.current-value').find('span').text();
 
-        $(this).parents('.custom-select').find('.current-value').text(newCurrency);
+        $(this).parents('.custom-select').find('.current-value').find('span').text(newCurrency);
         $(this).text(oldCurrency);
         $(this).parents('.custom-select').find('input').attr('value', newCurrency);
 
@@ -83,45 +83,12 @@ $(window).ready(function() {
 
     });
 
-    $('.slider-banner').slick({
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 981,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 640,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
-
-    $('.products-slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        appendArrows: '.products-control',
-        prevArrow: '.products-control-prev',
-        nextArrow: '.products-control-next',
-        responsive: [
-            {
-                breakpoint: 481,
-                settings: {
-                    slidesToShow: 3
-                }
-            }
-        ]
-    });
-
     $('.catalog-burger').click(function() {
         $(this).parents('.catalog').toggleClass('active');
+    });
+
+    $('.aside-burger').click(function() {
+        $(this).parents('aside').toggleClass('active');
     });
 
     $('.products-main-prev').click(function() {
@@ -146,6 +113,81 @@ $(window).ready(function() {
                 $('.product-card:first-of-type').css('margin-left', margin - 234 + 'px');
             }
         }
+    });
+
+    $('.slider-banner').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 981,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.page-main .products-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        appendArrows: '.products-control',
+        prevArrow: '.products-control-prev',
+        nextArrow: '.products-control-next',
+        responsive: [
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 3
+                }
+            }
+        ]
+    });
+
+    $('.page-inner .products-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        appendArrows: '.products-control',
+        prevArrow: '.products-control-prev',
+        nextArrow: '.products-control-next'
+    });
+
+    /***********************/
+   /* filter-cost scripts */
+  /***********************/
+
+    var filterCost = $("#filter-cost-item");
+    var minCost = $('#min-cost');
+    var maxCost = $('#max-cost');
+
+    filterCost.rangeSlider({
+        bounds:{min: 0, max: 5000},
+        defaultValues:{min: 500 , max: 2700},
+        step: 50
+    });
+
+    filterCost.bind("valuesChanging", function(e, data){
+        minCost.val( data.values.min );
+        maxCost.val( data.values.max );
+    });
+
+    minCost.val( filterCost.rangeSlider('min') );
+    maxCost.val( filterCost.rangeSlider('max') );
+
+    minCost.change(function() {
+        filterCost.rangeSlider("min", $(this).val());
+    });
+
+    maxCost.change(function() {
+        filterCost.rangeSlider("max", $(this).val());
     });
 
 });
